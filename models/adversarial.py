6eecb2c44b3bb372a02fe23f35b38cc66ae9dee3
Matthew <mradovan@stanford.edu>
2020-05-03 03:31:16 -0600

    some more fleshing out; a few notes on assumptions being made

--- models/adversarial.py
+++ models/adversarial.py
@@ -3,7 +3,16 @@ import tensorflow as tf
 
 from tensorflow import keras
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Concatenate, LSTM, Conv2D, LeakyReLU
+from tensorflow.keras.layers import (
+    Input,
+    Dense,
+    Reshape,
+    Flatten,
+    Concatenate,
+    LSTM,
+    Conv2D,
+    LeakyReLU
+)
 
 # Call model = AdversarialModel(GeneratorModelName(), generator)
 class AdversarialModel():
@@ -13,9 +22,12 @@ class AdversarialModel():
         self.data = data
         self.name = name
         self.discriminator = None
+        self.full_model = None
         self.build_discriminator()
+        self.connect_generator()
         self.show_model_structures()
 
+    # so many arbitrary design choices here rn.  very easy to modify.
     def build_discriminator(self):
         img_input = Input(shape=(250, 250, 3))
         for filters in [32, 32, 64, 64, 32, 32, 16]: # pretty arbitrary right now
@@ -40,6 +52,10 @@ class AdversarialModel():
 
         self.discriminator = keras.Model(inputs=[img_input, caption_input], outputs=[combined], name='adversarial_caption')
 
+    def connect_generator(self):
+        # i have no idea if the inputs/outputs works but this is the genearl idea.
+        self.full_model = keras.Model(inputs=self.generator.inputs, outputs=self.generator.outputs + self.discriminator.outputs)
+
     def show_model_structures(self):
         if self.discriminator:
             keras.utils.plot_model(self.discriminator, 'discriminator_{}.png'.format(self.name))
@@ -52,6 +68,8 @@ class AdversarialModel():
         real = np.ones((batch_size,))
         fake = np.zeros((batch_size,))
         for epoch in range(epochs):
+            # design choice - this iterates thru N individually per epoch; do we want to
+            # not iterate and just batch? let's find out thru speed tests...
             for batch_idx, (images, real_captions) in enumerate(self.data(batch_size=batch_size)):
                 fake_captions = self.generator.predict(images)
 
@@ -60,8 +78,9 @@ class AdversarialModel():
 
                 discriminator_loss = np.mean([real_loss, fake_loss], axis=0)
 
+                # this also makes assumptions; i assume they're justified in the pix2pix paper.
+                # we can also freeze the discriminator model's weights before training the combined model.
                 # https://github.com/eriklindernoren/Keras-GAN/blob/master/pix2pix/pix2pix.py
-                generator_loss = self.combined.train_on_batch([images, real_captions], [valid, real_captions])
+                generator_loss = self.full_model.train_on_batch([images, real_captions], [real_captions, valid])
 
                 print('Epoch {}/{}:\n\tD Loss {}, acc {}\n\tG Loss {}'.format(epoch, epochs, discriminator_loss, ))
-    
