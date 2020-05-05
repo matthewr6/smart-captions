2e6d8013108f61a1e65ba10b857e2d8025cb502c
Matthew <mradovan@stanford.edu>
2020-05-05 14:43:53 -0600

    modify data generator to work with vectorized text

--- models/data_generator.py
+++ models/data_generator.py
@@ -3,15 +3,27 @@ import pandas as pd
 import numpy as np
 from glob import glob
 import os
-# from keras.preprocessing import image as krs_image
 
-# Create an empty data generator
-# datagen = ImageDataGenerator()
+# TODO: ImageDataGenerator with flipping/rotating/etc?
+
+# Note: longest caption is 25.  So, let's pad to 30.
+PAD_SIZE = 30
+EOM = -1
+def load_captions():
+    with open('../data/captions.txt') as f:
+        data = f.readlines()
+    loaded = {}
+    for example in data:
+        example = example[:-1].split(',')
+        img_idx = example[0]
+        word_vector = [int(w) for w in example[1:]]
+        word_vector.append(EOM)
+        loaded[img_idx] = np.pad(word_vector, (0, PAD_SIZE - len(word_vector)))
+    return loaded
 
 # Read the image list and csv
 image_list = glob('../data/processed/*.*')
-df = pd.read_csv('../data/captions.csv', header=None, names=['id', 'caption'])
-df.set_index('id', inplace=True)
+data = load_captions()
 
 img_height = 400
 img_width = 400
@@ -30,7 +42,8 @@ def data_generator(batch_size=100):
             image = np.load(image_path)
 
             # Read data from csv using the name of current image
-            caption = df.loc[int(image_name)][0]
+            # caption = df.loc[int(image_name)][0]
+            caption = data[image_name]
 
             batch['images'].append(image)
             # print(image.shape)
@@ -44,6 +57,7 @@ def data_generator(batch_size=100):
         yield [batch['images'], batch['captions']]
 
 if __name__ == '__main__':
-    gen = generator(batch_size=100)
-    n = next(gen)
-    print(n[0].shape, n[1].shape)
+    gen = data_generator(batch_size=1000)
+    for i in range(10):
+        n = next(gen)
+        print(n[0].shape, n[1].shape)
