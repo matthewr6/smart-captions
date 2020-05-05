2b9e5fb767ca44905931c9dca8446c1bb14076b7
antonioferris <antonioferris01@gmail.com>
2020-05-04 20:16:29 -0700

    Added get_caption to reduce_captions

--- preprocessing/reduce_captions.py
+++ preprocessing/reduce_captions.py
@@ -1,13 +1,25 @@
+import csv
 
 
-
-
+def get_captions(filename):
+    """
+        Return the captions from the csv filename as a dictionary
+        from index to caption as a string.
+    """
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        lines = list(reader)
+        lines = [(int(idx), caption) for idx, caption in lines]
+    
+    return dict(lines)
 
 
 
 
 def main():
-    print('hi')
+    caption_file = '../data/captions.csv'
+    captions = get_captions(caption_file)
+
 
 if __name__ == '__main__':
     main()
