dc13866f43e3265723a063c3ae8e56cc94c86efc
Matthew <mradovan@stanford.edu>
2020-05-05 17:36:12 -0600

    not sure what happened, mb

--- models/genv1+adv1.py
+++ models/genv1+adv1.py
@@ -1,4 +1,7 @@
+from data_generator import data_generator
 from adversarial_v1 import AdversarialModelV1
 from single_stage_generator_v1 import SingleStageGeneratorV1
 
 model = AdversarialModelV1(SingleStageGeneratorV1(), 'adv1+ssgv1')
+
+model.train(data_generator(batch_size=100))
